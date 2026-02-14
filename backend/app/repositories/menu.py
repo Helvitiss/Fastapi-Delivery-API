e02751dd99ab81f413a9cd9e1bbfba82d09ac0a2
hellv <hellvitiss@gmail.com>
2026-02-14 13:20:28 +0300

    Обновил репо меню

--- backend/app/repositories/menu.py
+++ backend/app/repositories/menu.py
@@ -1,86 +1,79 @@
-from typing import Sequence
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import DishModel, CategoryModel
-from app.schemas.category import CategoryCreate, CategoryUpdate
-from app.schemas.dish import DishCreate, DishUpdate
 
-
-class DishRepository:
-    def __init__(self, db: AsyncSession):
-        self.db = db
-
-    async def get_all(self)-> Sequence[DishModel]:
-        result = await self.db.scalars(select(DishModel))
-
-        return result.all()
-
-
-    async def get_by_id(self, dish_id) -> DishModel | None:
-        stmt = select(DishModel).where(DishModel.id == dish_id)
-        result = await self.db.scalars(stmt)
-        return result.first()
-
-
-    async def create(self, dish: DishCreate) -> DishModel:
-        new_dish = DishModel(**dish.model_dump())
-        self.db.add(new_dish)
-        await self.db.commit()
-        await self.db.refresh(new_dish)
-        return new_dish
-
-    async def update(self, dish_id: int, new_dish: DishUpdate | DishCreate):
-        dish = await self.get_by_id(dish_id)
-        for k, v in new_dish.model_dump(exclude_unset=True).items():
-            setattr(dish, k, v)
-
-        await self.db.commit()
-        await self.db.refresh(dish)
-        return dish
-
-    async def delete(self, dish_id: int) -> DishModel | None:
-        dish = await self.get_by_id(dish_id)
-        if dish is None:
-            return None
-        await self.db.delete(dish)
-        await self.db.commit()
-        return dish
 
 
 class CategoryRepository:
-    def __init__(self, db: AsyncSession):
-        self.db = db
+    def __init__(self, session: AsyncSession):
+        self.session = session
 
-    async def get_all(self) -> Sequence[CategoryModel]:
-        result = await self.db.scalars(select(CategoryModel))
+    async def create(self, category: dict) -> CategoryModel:
+        new_category = CategoryModel(**category)
+        self.session.add(new_category)
+        await self.session.flush()
+        return new_category
+
+
+    async def get_all(self) -> list[CategoryModel]:
+        result = await self.session.scalars(select(CategoryModel))
         return result.all()
-
 
     async def get_by_id(self, category_id) -> CategoryModel:
         stmt = select(CategoryModel).where(CategoryModel.id == category_id)
-        result = await self.db.scalar(stmt)
+        result = await self.session.scalar(stmt)
+        if result is None:
+            raise ValueError(f"Category not found: {category_id}")
         return result
 
-    async def create(self, category: CategoryCreate) -> CategoryModel:
-        new_category = CategoryModel(**category.model_dump())
-        self.db.add(new_category)
-        await self.db.commit()
-        await self.db.refresh(new_category)
-        return new_category
-
-    async def update(self, category_id: int, new_category: CategoryUpdate) -> CategoryModel:
+    async def update(self, category_id: int, new_category: dict) -> CategoryModel:
         category = await self.get_by_id(category_id)
-        for k, v in new_category.model_dump(exclude_unset=True).items():
+        for k, v in new_category.items():
             setattr(category, k, v)
-        await self.db.commit()
-        await self.db.refresh(category)
+        await self.session.flush()
         return category
 
 
-    async def delete(self, category_id: int):
+    async def delete(self, category_id: int) -> None:
         category = await self.get_by_id(category_id)
-        await self.db.delete(category)
-        await self.db.commit()
-        return category
+        await self.session.delete(category)
+
+
+class DishRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create(self, dish: dict) -> DishModel:
+        new_dish = DishModel(**dish)
+        self.session.add(new_dish)
+        await self.session.flush()
+        return new_dish
+
+
+    async def get_by_id(self, dish_id: int) -> DishModel:
+        stmt = select(DishModel).where(DishModel.id == dish_id)
+        result = await self.session.scalar(stmt)
+        if result is None:
+            raise ValueError(f"Dish not found: {dish_id}")
+        return result
+
+
+    async def get_all(self)-> list[DishModel]:
+        result = await self.session.scalars(select(DishModel))
+        return result.all()
+
+    async def update(self, dish_id: int, new_dish: dict) -> DishModel:
+        dish = await self.get_by_id(dish_id)
+        for k, v in new_dish.items():
+            setattr(dish, k, v)
+
+        await self.session.flush()
+        return dish
+
+    async def delete(self, dish_id: int) -> None:
+        dish = await self.get_by_id(dish_id)
+
+        await self.session.delete(dish)
+
