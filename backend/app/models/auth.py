f4e8d97b9268c865e9d59db3c605459b2dce7d3e
hellv <hellvitiss@gmail.com>
2026-02-14 12:31:02 +0300

    Переделал модели пользователя и авторизации

---
+++ backend/app/models/auth.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.database import Base
+
+
+
+class OTPCode(Base):
+    __tablename__ = 'otp_codes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    phone_number: Mapped[str] = mapped_column(String(20), index=True)
+    code: Mapped[str] = mapped_column(String(6))
+    expires_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,15 +3,15 @@ from datetime import datetime
 from app.core.database import Base
 from sqlalchemy import String, Text, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
+from app.models.enums import UserRole
 
 class UserModel(Base):
     __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
-    email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True, index=True)
-    hashed_password: Mapped[str] = mapped_column(String(50), nullable=False)
-    is_active: Mapped[bool] = mapped_column(default=True)
+
+    role: Mapped[UserRole] = mapped_column(default=UserRole.USER)
+
 
     cart: Mapped["CartModel"] = relationship("CartModel", back_populates="user")
     orders: Mapped[list["OrderModel"]] = relationship("OrderModel", back_populates="user")
