9d44a3d547855244c4a23eff6244c181c8489ff5
Aleksandr Korabel <74402513+Helvitiss@users.noreply.github.com>
2026-03-03 18:04:16 +0300

    убрал лишний принт

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -13,7 +13,6 @@ import app.models
 
 
 
-print("METADATA TABLES:", list(Base.metadata.tables.keys()))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
