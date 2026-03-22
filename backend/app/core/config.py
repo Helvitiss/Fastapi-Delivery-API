f42cb97345f9c506004d28c2c723c779d278708a
helvitiss <hellvitiss@gmail.com>
2026-03-22 13:29:15 +0300

    Инициализировал Celery

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -20,7 +20,8 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
     OTP_EXPIRE_MINUTES: int = 5
 
-
+    CELERY_BROKER_URL: str = "redis://localhost:6379/0"
+    CELERY_RESULT_BACKEND: str = "redis://localhost:6379/1"
 
     model_config = SettingsConfigDict(
         env_file= BASE_DIR / ".env",
